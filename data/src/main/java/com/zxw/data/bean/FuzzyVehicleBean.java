7f1ecdcab6956331435fa7d908af0d74e105b780
cangjie2016@gmail.com <cangjie2016@gmail.com>
2017-03-02 19:20:59 +0800

    优化停班车辆手动进站对话窗

---
+++ data/src/main/java/com/zxw/data/bean/FuzzyVehicleBean.java
@@ -0,0 +1,26 @@
+package com.zxw.data.bean;
+
+/**
+ * author：CangJie on 2017/3/2 19:05
+ * email：cangjie2016@gmail.com
+ */
+public class FuzzyVehicleBean {
+    private String code;
+    private int vehicleId;
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public int getVehicleId() {
+        return vehicleId;
+    }
+
+    public void setVehicleId(int vehicleId) {
+        this.vehicleId = vehicleId;
+    }
+}
