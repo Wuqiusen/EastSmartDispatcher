2875c4b9166e89ee0f92e0ee7a3e5d65f64be705
cangjie2016@gmail.com <woaini123>
2017-05-20 09:28:37 +0800

    编写接口

--- data/src/main/java/com/zxw/data/http/HttpMethods.java
+++ data/src/main/java/com/zxw/data/http/HttpMethods.java
@@ -4,6 +4,7 @@ import com.zxw.data.bean.BackHistory;
 import com.zxw.data.bean.BaseBean;
 import com.zxw.data.bean.ChangePwdBean;
 import com.zxw.data.bean.DepartCar;
+import com.zxw.data.bean.DriverWorkloadItem;
 import com.zxw.data.bean.FuzzyVehicleBean;
 import com.zxw.data.bean.InformContentBean;
 import com.zxw.data.bean.InformDataBean;
@@ -39,8 +40,8 @@ import rx.schedulers.Schedulers;
  * email：cangjie2016@gmail.com
  */
 public class HttpMethods {
-//    public static final String BASE_URL = "http://192.168.0.50:8081/yd_control_app/";
-    public static final String BASE_URL = "http://120.77.48.103:8080/yd_control_app/";
+    public static final String BASE_URL = "http://192.168.0.50:8081/yd_control_app/";
+//    public static final String BASE_URL = "http://120.77.48.103:8080/yd_control_app/";
 //    public static final String BASE_URL = "http://150970t1u9.51mypc.cn:52222/yd_control_app/";// 测试
     public Retrofit retrofit = RetrofitSetting.getInstance();
 
@@ -435,5 +436,16 @@ public class HttpMethods {
         toSubscribe(map,subscriber);
     }
 
-
+    // 获取工作量列表
+    public void workloadList(Subscriber<List<DriverWorkloadItem>> subscriber, String userId, String keyCode, int lineId, int pageNo, int pageSize, String vehCode, String driverName){
+        HttpInterfaces.Browse browse = retrofit.create(HttpInterfaces.Browse.class);
+        Observable<List<DriverWorkloadItem>> map = browse.workloadList(userId, keyCode, lineId, pageNo, pageSize, vehCode, driverName).map(new HttpResultFunc<List<DriverWorkloadItem>>());
+        toSubscribe(map,subscriber);
+    }
+    // 修改工作量审核记录
+    public void updateWorkload(Subscriber subscriber,String userId,String keyCode,long objId,String outTime,String arrivalTime,int gpsStatus,int opStatus){
+        HttpInterfaces.Operator operator = retrofit.create(HttpInterfaces.Operator.class);
+        Observable map = operator.updateWorkload(userId, keyCode, objId, outTime, arrivalTime, gpsStatus, opStatus);
+        toSubscribe(map,subscriber);
+    }
 }
